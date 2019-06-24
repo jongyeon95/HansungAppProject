06e1e5133aca58eaa7bfb9fc17f764ff513cc21b
jongyeon <whddus9509@naver.com>
2019-06-25 00:36:55 +0900

    06/25 12:36

---
+++ HansingQuickSeat/app/src/main/java/com/example/com/hansungquickseat/ReserveActivity.java
@@ -0,0 +1,18 @@
+package com.example.com.hansungquickseat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by com on 2019-06-25.
+ */
+
+public class ReserveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
