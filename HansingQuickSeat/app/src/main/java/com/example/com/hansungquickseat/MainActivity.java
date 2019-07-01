f0ea908b6b7cb3997f4dce622d7dd56ee9e9758c
jongyeon <whddus9509@naver.com>
2019-07-01 23:56:34 +0900

    7월1일 레이아웃 및 웹뷰
    레이아웃 및 웹뷰 틀

--- HansingQuickSeat/app/src/main/java/com/example/com/hansungquickseat/MainActivity.java
+++ HansingQuickSeat/app/src/main/java/com/example/com/hansungquickseat/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.com.hansungquickseat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton=(Button)findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ReserveActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
+
 }
--- HansingQuickSeat/app/src/main/java/com/example/com/hansungquickseat/ReserveActivity.java
+++ HansingQuickSeat/app/src/main/java/com/example/com/hansungquickseat/ReserveActivity.java
@@ -3,16 +3,52 @@ package com.example.com.hansungquickseat;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by com on 2019-06-25.
  */
 
 public class ReserveActivity extends AppCompatActivity {
+    private WebView mWebView;
+    private  String myURL="https://hsel.hansung.ac.kr/home_login_write.mir";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.webviewlayout);
+        // 웹뷰 셋팅팅
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        //mWebView.loadUrl("http://www.pois.co.kr/mobile/login.do");
+
+        mWebView.loadUrl(myURL + "/html/test.html"); // 접속 URL
+        mWebView.setWebChromeClient(new WebChromeClient());
+        mWebView.setWebViewClient(new WebViewClientClass());
+
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.d("check URL",url);
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
 }
 
