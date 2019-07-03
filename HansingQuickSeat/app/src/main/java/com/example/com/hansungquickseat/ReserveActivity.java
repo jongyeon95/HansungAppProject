5b15deb62931c94e801e8d102e37c4b61e51645c
jongyeon <whddus9509@naver.com>
2019-07-03 23:06:18 +0900

    ...로그인 실패

--- HansingQuickSeat/app/src/main/java/com/example/com/hansungquickseat/ReserveActivity.java
+++ HansingQuickSeat/app/src/main/java/com/example/com/hansungquickseat/ReserveActivity.java
@@ -6,8 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * Created by com on 2019-06-25.
@@ -15,7 +23,11 @@ import android.webkit.WebViewClient;
 
 public class ReserveActivity extends AppCompatActivity {
     private WebView mWebView;
-    private  String myURL="https://hsel.hansung.ac.kr/home_login_write.mir";
+    private  String myURL="http://hsel.hansung.ac.kr/";
+    String postData ="home_login_mloc_code="+"HSEL"+"&home_login_id=1494017"+"&home_login_password=!qweasd9577"+"&home_login_id_save_yn=N";
+    String str;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +35,20 @@ public class ReserveActivity extends AppCompatActivity {
         // 웹뷰 셋팅팅
         mWebView = (WebView) findViewById(R.id.webView);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+
         //mWebView.loadUrl("http://www.pois.co.kr/mobile/login.do");
 
-        mWebView.loadUrl(myURL); // 접속 URL
+        try {
+            str="home_login_mloc_code="+ URLEncoder.encode("HSEL","UTF-8")+"&home_login_id="+ URLEncoder.encode("아이디","UTF-8")+"&home_login_password="+ URLEncoder.encode("비밀번호","UTF-8")+"&home_login_id_save_yn="+ URLEncoder.encode("N","UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        mWebView.loadUrl(myURL);
+        mWebView.postUrl("https:/https://hsel.hansung.ac.kr/home_security_login_write_prss.mir",str.getBytes());
         mWebView.setWebChromeClient(new WebChromeClient());
         mWebView.setWebViewClient(new WebViewClientClass());
+        mWebView.loadUrl(myURL); // 접속 URL
 
     }
 
